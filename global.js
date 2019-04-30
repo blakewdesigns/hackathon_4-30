14de455ae4b6037d0ac0c0bbd1c930eb8eb20da2
Blake Walker <blake.wdesigns@gmail.com>
2019-04-30 12:02:24 -0400

    javascript added

---
+++ global.js
@@ -0,0 +1,32 @@
+let todos = [
+    {
+        description: "Go to school"
+    },
+    {
+        description: "Go to work"
+    }
+]
+
+btn = document.querySelector("#btn")
+field = document.querySelector('#input')
+ul = document.querySelector("#list")
+
+renderTodos()
+
+function renderTodos() {
+    todos.forEach(todo =>{
+        li = document.createElement("li")
+        li.innerText = todo.description
+
+        ul.appendChild(li)
+    })
+}
+
+btn.addEventListener('click', addTodo)
+
+function addTodo() {
+    field = document.querySelector('#input')
+    li = document.createElement('li')
+    newTodo = {description: field.value}
+    todos.push(newTodo)
+}
